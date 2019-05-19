9e952332efdb98c97ad5af55912848c263b7646d
jip <dev@guitaresphere.Com>
2019-05-19 14:55:51 +0200

    feature Delete done

--- src/Game.java
+++ src/Game.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Scanner;
 
 class Game {
+
     // liste des Entitys
-    private List<Entity> entity  = new ArrayList<>();
+    List<Entity> entities = new ArrayList<>();
     /**
      * Entry point   : method static  playGame
      */
@@ -15,10 +16,10 @@ class Game {
 
     }
     private void showMenu(Game game){
-         clearScreen();
+        clearScreen();
         Scanner sc = new Scanner(System.in);
         System.out.println("_______________________________________________________________________________________________________________" );
-        System.out.println("Ajouter une Entité :: 1 | Liste Entités :: 2 | éditer une Entité :: 3 | Parcourir le donjon :: 4 | Quitter :: 5" );
+        System.out.println("Ajouter une Entité :: 1 | Liste Entités :: 2 | éditer une Entité :: 3 | Supprimer une entité :: 4 | Quitter :: 5" );
         System.out.println("_______________________________________________________________________________________________________________" );
         String str = sc.nextLine();
 
@@ -32,28 +33,48 @@ class Game {
                   game.listeEntity(game);
                 break;
                 case 3:
+                 // sub menu edition ENtity
                  game.editEntity(game);
                  break;
                 case 4:
-                case 5:
+                    game.deleteEntity(game);
                     break;
+                default:
+
             }
             clearScreen();
             System.out.println("_______________________________________________________________________________________________________________" );
-            System.out.println("Ajouter une Entité :: 1 | Liste Entités :: 2 | éditer une Entité :: 3 | Parcourir le donjon :: 4 | Quitter :: 5" );
+            System.out.println("Ajouter une Entité :: 1 | Liste Entités :: 2 | éditer une Entité :: 3 | Supprimer une entité :: 4 | Quitter :: 5" );
             System.out.println("_______________________________________________________________________________________________________________" );
             str = sc.nextLine();
         }
     }
     private void listeEntity(Game game){
 
-        for (Entity hero: game.entity) {
+        for (Entity hero: game.entities) {
             hero.showInfo();
         }
     }
     private void AddEntity(Game game){
         Entity NewEntity = game.createEntity();
-        this.entity.add(NewEntity);
+        this.entities.add(NewEntity);
+    }
+    private Entity createEntity(){
+        clearScreen();
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenue dans Game choisissez un Nom pour votre personnage  : " );
+        name = sc.nextLine();
+        System.out.println(" L'archétype  de votre personnage :: 1 pour " + TypeEntity.GUERRIER + "  |  2 pour  " + TypeEntity.MAGICIEN + " ? :");
+        String str = sc.nextLine();
+
+        if (Integer.parseInt(str) == 1 ){
+            System.out.println("Vous avez choisi : " + TypeEntity.GUERRIER);
+            return new Guerrier(name ,TypeEntity.GUERRIER.minHealth,TypeEntity.GUERRIER.maxHealth,TypeEntity.GUERRIER.minStrength,TypeEntity.GUERRIER.maxStrength);
+        }else{
+            System.out.println("Vous avez choisi : " + TypeEntity.MAGICIEN);
+            return  new Magicien(name ,TypeEntity.MAGICIEN.minHealth,TypeEntity.MAGICIEN.maxHealth,TypeEntity.MAGICIEN.minStrength,TypeEntity.MAGICIEN.maxStrength);
+        }
     }
     private void editEntity(Game game){
         int i = 0;
@@ -64,7 +85,7 @@ class Game {
         System.out.println("EDITION   ");
         System.out.println("__________");
 
-        for (Entity hero: game.entity) {
+        for (Entity hero: game.entities) {
            System.out.print( hero.getNom() + " Tapez " + i + "  |  ");
            i++;
         }
@@ -75,12 +96,14 @@ class Game {
             Game.clearScreen();
 
             // formulaire edition avec index
-            game.entity.get(Integer.parseInt(str)).editEntity();
+            game.entities.get(Integer.parseInt(str)).editEntity();
 
 
             i = 0;
-            System.out.println("Editer : ");
-            for (Entity hero: game.entity) {
+            System.out.println("__________");
+            System.out.println("EDITION   ");
+            System.out.println("__________");
+            for (Entity hero: game.entities) {
                 System.out.print( hero.getNom() + " Tapez " + i + "  |  ");
                 i++;
             }
@@ -91,23 +114,47 @@ class Game {
 
 
     }
-    private Entity createEntity(){
-       clearScreen();
-       String name;
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Bienvenue dans Game choisissez un Nom pour votre personnage  : " );
-       name = sc.nextLine();
-       System.out.println(" L'archétype  de votre personnage :: 1 pour " + TypeEntity.GUERRIER + "  |  2 pour  " + TypeEntity.MAGICIEN + " ? :");
-       String str = sc.nextLine();
+    private void deleteEntity(Game game){
+        int i = 0;
+        String str;
+        Scanner sc = new Scanner(System.in);
 
-       if (Integer.parseInt(str) == 1 ){
-            System.out.println("Vous avez choisi : " + TypeEntity.GUERRIER);
-            return new Guerrier(name ,TypeEntity.GUERRIER.minHealth,TypeEntity.GUERRIER.maxHealth,TypeEntity.GUERRIER.minStrength,TypeEntity.GUERRIER.maxStrength);
-       }else{
-            System.out.println("Vous avez choisi : " + TypeEntity.MAGICIEN);
-            return  new Magicien(name ,TypeEntity.MAGICIEN.minHealth,TypeEntity.MAGICIEN.maxHealth,TypeEntity.MAGICIEN.minStrength,TypeEntity.MAGICIEN.maxStrength);
-       }
-   }
+        System.out.println("__________");
+        System.out.println("DELETE    ");
+        System.out.println("__________");
+
+        for (Entity hero: game.entities) {
+            System.out.print( hero.getNom() + " Tapez " + i + "  |  ");
+            i++;
+        }
+        System.out.println("Quittez le mode Delete :: q ");
+        str = sc.nextLine();
+
+        while (!str.equals("q")){
+            Game.clearScreen();
+
+            //delete Entity
+            game.entities.remove(Integer.parseInt(str));
+
+
+            i = 0;
+            System.out.println("__________");
+            System.out.println("DELETE    ");
+            System.out.println("__________");
+            for (Entity hero: game.entities) {
+                System.out.print( hero.getNom() + " Tapez " + i + "  |  ");
+                i++;
+            }
+            System.out.println("Quittez le mode Delete :: q ");
+            str = sc.nextLine();
+
+        }
+
+
+
+
+
+    }
     private static void clearScreen() {
 
         System.out.print("\033[H\033[2J");
