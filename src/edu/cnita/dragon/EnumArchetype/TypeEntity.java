6b8a904541be5d84c986c8a20fc82530d3af3a39
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-05-21 15:56:58 +0200

    UI Console affichage uniquement des données (80% )

--- src/edu/cnita/dragon/EnumArchetype/TypeEntity.java
+++ src/edu/cnita/dragon/EnumArchetype/TypeEntity.java
@@ -1,17 +1,20 @@
 package edu.cnita.dragon.EnumArchetype;
 
 public enum TypeEntity {
-    GUERRIER (5,10,5,10),
-    MAGICIEN (3,6,8,15);
+    GUERRIER (5,10,5,10,"Bouclier"),
+    MAGICIEN (3,6,8,15,"Philtre");
 
-    TypeEntity(int minH,int maxH,int minS,int maxS) {
+    TypeEntity(int minH,int maxH,int minS,int maxS,String defense) {
         this.minHealth   = minH;
         this.maxHealth   = maxH;
         this.minStrength = minS;
         this.maxStrength = maxS;
+        this.defense = defense;
     }
     public final int minHealth;
     public final int maxHealth;
     public final int minStrength;
     public final int maxStrength;
+    public final String defense;
+
 }
